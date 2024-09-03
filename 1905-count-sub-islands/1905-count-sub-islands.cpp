4797bac54da70f1e6ed37f3fe1904e7cf2d64f96
SiddharthGaraiya <125427796+SiddharthGaraiya@users.noreply.github.com>
2024-09-03 22:39:04 +0530

    Time: 211 ms (59.75%), Space: 96.5 MB (49.71%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    //DFS
+    bool checkSubIsland(vector<vector<int>>& grid1, vector<vector<int>>& grid2,int i ,int j)
+    {
+        if (i<0|| i>=grid1.size() || j<0 || j>=grid2[0].size()){
+            return true;
+        }
+        if (grid2[i][j]!=1)
+            return true;
+        
+        grid2[i][j]=-1; //mark visited
+        
+        bool result = (grid1[i][j]==1);
+        
+        result = result & checkSubIsland(grid1,grid2,i+1,j);
+        result = result & checkSubIsland(grid1,grid2,i-1,j);
+        result = result & checkSubIsland(grid1,grid2,i,j+1);
+        result = result & checkSubIsland(grid1,grid2,i,j-1);
+        
+        return result;
+    }
+    
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int subIslands= 0;
+        int m = grid2.size();           //no of rows
+        int n = grid2[0].size();        //no of cols
+        
+        for (int i =0;i<m;i++){
+            for (int j=0;j<n;j++){
+                if (grid2[i][j] ==1 && checkSubIsland(grid1,grid2,i,j)){
+                    subIslands++;
+                }
+            }
+        }
+        
+        return subIslands;
+    }
+};
